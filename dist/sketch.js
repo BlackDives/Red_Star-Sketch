3e8572cd77605e01dea1e115b95908c8ca4563c9
BlackDives <sirkamronwilson00@gmail.com>
2022-01-24 19:33:41 -0600

    initial commit

---
+++ dist/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+  background(44, 30, 178)
+  
+  fill(30, 178, 44)
+  stroke(255)
+  strokeWeight(5)
+  circle(200, 200, 250)
+  
+  stroke(255)
+  fill(255, 0, 0)
+  beginShape()
+  vertex(200, 75)
+  vertex(160, 160)
+  vertex(75, 160)
+  vertex(140, 220)
+  vertex(125, 300)
+  vertex(200, 250)
+  vertex(275, 300)
+  vertex(255, 220)
+  vertex(325, 160)
+  vertex(240, 160)
+  vertex(200, 75)
+  endShape()
+}
+
